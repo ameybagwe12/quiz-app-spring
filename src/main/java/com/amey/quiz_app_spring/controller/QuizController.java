8ec4e9faf6fa7f2c09a1581c3e1d0e62754d812a
Amey Bagwe <ameybagwe09@gmail.com>
2024-10-18 12:22:58 +0530

    Question Wrapper added and fetched

--- src/main/java/com/amey/quiz_app_spring/controller/QuizController.java
+++ src/main/java/com/amey/quiz_app_spring/controller/QuizController.java
@@ -1,13 +1,14 @@
 package com.amey.quiz_app_spring.controller;
 
+import com.amey.quiz_app_spring.model.Question;
+import com.amey.quiz_app_spring.model.QuestionWrapper;
 import com.amey.quiz_app_spring.service.QuizService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("quiz")
@@ -19,5 +20,10 @@ public class QuizController {
     @PostMapping("create")
     public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
         return quizSer.createQuiz(category, numQ, title);
+    }
+
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizSer.getQuizQuestions(id);
     }
 }
